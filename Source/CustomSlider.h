cc4977043e4d4e8938bf39140d9ae5c89e2d1078
ishwar <ishwarhegde10@gmail.com>
2025-11-30 00:10:36 +0530

    shift modifier for finetuning with scroll wheel

--- Source/CustomSlider.h
+++ Source/CustomSlider.h
@@ -4,11 +4,6 @@
 
 struct superSlider:public juce::Slider
 {
-    using Slider = juce::Slider;
-    using Style = Slider::SliderStyle;
-    using BoxPos = Slider::TextEntryBoxPosition;
-    using Mouse = juce::MouseEvent;
-    using Wheel = juce::MouseWheelDetails;
     static constexpr float normalWheel = .7f;
     static constexpr float sensitiveWheel = 0.1f;
 
